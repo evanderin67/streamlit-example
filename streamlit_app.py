83a569b4238eb342edcf6cb33cd7aa603b0f7d63
Evan Derin Ihsanudin <70137047+evanderin67@users.noreply.github.com>
2023-02-19 12:59:49 +0700

    first_commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import eda
+import prediction
 
-"""
-# Welcome to Streamlit!
+# Set Config dan icon
+st.set_page_config(
+        page_title='Churn Prediction',
+        layout='wide',
+        )
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Hide Streamlit Style
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#Membuat navigasi
+st.sidebar.markdown("# Evan Derin Ihsanudin - RMT-FTDS-17")
+navigation = st.sidebar.selectbox('Pilih Halaman (Churn Prediction/EDA): ', ('Churn Prediction','Exploratory Data Analysis'))
+st.sidebar.image("https://imgur.com/t4aS0jH.png", use_column_width=True)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#Run modul dengan if else
+if navigation == 'Churn Prediction' :
+    prediction.run()
+else :
+    eda.run()
